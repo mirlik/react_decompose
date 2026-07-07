2a4d1e876d4ada1fe86fbf1f70cc2ddc1d3c8200
Alex <11165950+mirlik@users.noreply.github.com>
2026-07-07 16:53:55 +0300

    fix#2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 // import a css file containig header styles
 import './Header.scss';
-import Nav from '../Nav/Nav';
+import Navigation from '../Navigation/Navigation';
 
 // Create a Header function returning the HTML of header block
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <Nav />
+      <Navigation />
     </header>
   );
 }
