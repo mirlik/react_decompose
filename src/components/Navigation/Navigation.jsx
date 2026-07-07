af3fbe4724a71a34c74e31dd7180a3919378f296
Alex <11165950+mirlik@users.noreply.github.com>
2026-07-07 16:54:23 +0300

    fix#3

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+// import a css file containig header styles
+import './Navigation.scss';
+
+// Create a Header function returning the HTML of header block
+function Nav() {
+  return (
+    <nav className="nav">
+      <a className="navigation__link" href="#about">
+        About
+      </a>
+      <a className="navigation__link" href="#services">
+        Services
+      </a>
+      <a className="navigation__link" href="#contact">
+        Contact
+      </a>
+    </nav>
+  );
+}
+
+export default Nav;
